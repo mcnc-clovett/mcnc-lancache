51362d30ea3de655235bc18b23b02a149bd2a87d
Chris Lovett <clovett@mcnc.org>
2023-12-20 00:27:02 +0000

    Check if config exists and don't overwrite

--- copyconfig.sh
+++ copyconfig.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if mountpoint -q /etc/nginx; then
+if mountpoint -q /etc/nginx && [ ! -e /etc/nginx/nginx.conf ] ; then
 	  echo "Copying stock configuration to /etc/nginx"
 	    cp -an /etc/nginx.stock/* /etc/nginx
 fi
